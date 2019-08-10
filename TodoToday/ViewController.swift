6548e380599c08142bd25bd4adcfc8d3df04cd62
Everett <Everettsprenger@gmail.com>
2019-08-10 17:46:24 -0600

    Initial Commit

---
+++ TodoToday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoToday
+//
+//  Created by Everett  Sprenger on 2019-08-10.
+//  Copyright © 2019 Everett  Sprenger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
